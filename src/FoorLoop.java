a862170a6adcf9c1fd601fe540fac443f3197b77
Angui Clavijo Gutierrez <angiedesarrollo@gmail.com>
2023-01-29 17:50:12 -0500

    Revert "First part lerning Java"
    This reverts commit d6e9f53310f5f06a915d619a560cf1733e47d167.

--- src/FoorLoop.java
+++
@@ -1,7 +0,0 @@
-public class FoorLoop {
-    public static void main(String  args[]){
-        for(int x =  10; x < 20; x = x + 1) {
-            System.out.println("value of x:" + x);
-        }
-    }
-}
--- src/MethodToString.java
+++
@@ -1,9 +0,0 @@
-public class MethodToString {
-
-    public static void main(String args[]){
-        Integer x = 5;
-
-        System.out.println(x.toString() );
-        System.out.println(Integer.toString(12));
-    }
-}
